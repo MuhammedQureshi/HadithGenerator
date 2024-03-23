99e066c34dd3620312c301f6f9af9bb02792d456
Muhammed Qureshi <muhammed1qureshi@gmail.com>
2024-03-23 20:39:44 +0000

    Fetch Hadiths + make UI to display hadiths

--- app/components/hadith.tsx
+++ app/components/hadith.tsx
@@ -1,10 +1,21 @@
 'use client'
 import React from 'react'
-import { motion } from "framer-motion"
 
-const Hadith = () => {
+
+const Hadith = ({data}:any) => {
+
   return (
-    <div></div>
+    <div className='pt-[5rem] pb-[5rem]'>
+      <div className='px-5 text-2xl tracking-normal'>
+        <h1>{data.header}</h1>
+      </div>
+      <div className=' pt-10 px-8 text-3xl tracking-normal leading-[50px]'>
+        <p>{data.hadith_english}</p>
+      </div>
+      <div className='flex justify-end pt-10 px-8 text-3xl tracking-normal leading-[50px]'>
+        <p>({data.refno})</p>
+      </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
-import Image from "next/image";
-import { motion } from "framer-motion"
-import { PageWrapper } from "./components/page-wrapper";
+import { useEffect, useState } from 'react';
 
-export default function Home() {
+import { PageWrapper } from "./components/page-wrapper";
+import Hadith from "./components/hadith";
+
+async function Bukhari() {
+  const res = await fetch('https://random-hadith-generator.vercel.app/bukhari/');
+  const data = await res.json();
+
+  return data.data;
+}
+
+export default async function Home() {
+
+  const data = await Bukhari();
+
   return (
     <PageWrapper>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 customBg">
-      
-    </main>
+      <main className="flex bg5 min-h-screen flex-col items-center justify-center customBg">
+        <div className="container p-2 max-w-[1250px] h-screen">
+          <h1 className="flex p-4 text-3xl justify-center font-serif">Hadith a day</h1>
+          <Hadith data={data} />
+        </div>
+      </main>
     </PageWrapper>
   );
 }
